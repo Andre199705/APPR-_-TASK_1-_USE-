2b6f63c9d4ff1bc93c4bf4f80452c3cd31c2b3f9
Andre199705 <ST10118445@vcconnect.edu.za>
2022-10-14 20:29:00 +0200

    Data sets added

---
+++ APPR _ TASK_1 _USE!/Models/Categories.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APPR___TASK_1__USE_.Models
+{
+    public class Categories
+    {
+        [Key]
+
+        public int id { get; set; }
+        public string Category { get; set; }
+    }
+}
